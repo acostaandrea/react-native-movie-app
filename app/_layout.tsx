d1926a21529e569ae49f7ca7c191b713998665bd
Andrea Acosta <acostaandreadev@gmail.com>
2025-04-16 11:27:45 -0300

    feat: add movie API integration and now playing action, update dependencies

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,8 +1,9 @@
 import { View, Text } from 'react-native'
 import React from 'react'
 import "./global.css"
+import { NowPlayingAction } from '@/core/actions/movies/now-paying.action'
 
-const Layout = () => {
+const Layout = () => {  
   return (
     <View className='px-6'>
       <Text className='text-3xl'>Layout</Text>
