c4b5621629a07decfa0c6f67b8c6136c282b8641
Andrea Acosta <acostaandreadev@gmail.com>
2025-04-16 12:45:53 -0300

    feat: add MainSlideShow and MoviePoster components, implement loading indicator in HomeScreen. Installation of Carousel

--- app/home/index.tsx
+++ app/home/index.tsx
@@ -1,15 +1,22 @@
-import { View, Text } from "react-native";
+import { View, Text, ActivityIndicator } from "react-native";
 import React from "react";
 import { useMovies } from "@/presentation/hooks/useMovies";
+import { useSafeAreaInsets } from "react-native-safe-area-context";
+import MainSlideShow from "@/presentation/components/movies/MainSlideShow";
 
 const HomeScreen = () => {
+
+  const safeArea = useSafeAreaInsets();
   const { nowPlayingQuery } = useMovies();
+  if(nowPlayingQuery.isLoading) return(
+    <View className="flex-1 items-center justify-center ">
+      <ActivityIndicator color="purple" size={40}></ActivityIndicator>
+    </View>
+  ) 
   return (
-    <View>
-      <Text>HomeScreen</Text>
-      <Text>
-        {JSON.stringify(nowPlayingQuery.data, null, 2)}
-      </Text>
+    <View className="mt-2 " style={{ paddingTop: safeArea.top }}>
+      <Text className="text-3xl font-bold px-4 mb-2">Movies App</Text>
+      <MainSlideShow movies={nowPlayingQuery.data ?? []}/>      
     </View>
   );
 };
---
+++ presentation/components/movies/MainSlideShow.tsx
@@ -0,0 +1,34 @@
+import { View, Text, useWindowDimensions} from "react-native";
+import Carousel, { ICarouselInstance } from "react-native-reanimated-carousel";
+import React, { useRef } from "react";
+import { Movie } from "@/infrastructure/interfaces/movie.interface";
+import MoviePoster from "./MoviePoster";
+
+interface Props {
+  movies: Movie[];
+}
+
+const MainSlideShow = ({ movies }: Props) => {
+  const ref = useRef<ICarouselInstance>(null);
+  const width = useWindowDimensions().width;
+  return (
+    <View className="h-[250px] w-full">
+      <Carousel
+        data={movies}
+        renderItem={({ item }) => <MoviePoster id={item.id} poster={item.poster}/>}
+        ref={ref}
+        width={200}
+        height={350}
+        style={{ width, height: 350, justifyContent: "center", alignItems: "center" }}
+        mode="parallax"
+        modeConfig={{
+          parallaxScrollingScale: 0.9,
+          parallaxScrollingOffset: 50,
+        }}
+        defaultIndex={1}
+      />
+    </View>
+  );
+};
+
+export default MainSlideShow;
